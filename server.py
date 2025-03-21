6adb5f9ba366253860fb711f201e9fb66b598208
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-03-21 20:27:59 +0300

    Some updates

--- server.py
+++ server.py
@@ -1,19 +1,43 @@
+import threading
+
 from aiohttp import web
 
 
-async def receive_sms(request: web.Request):
-    data = await request.post()
+def _extract_code(content: str):
+    return "".join(list(filter(
+        lambda content: content.isdigit(),
+        content.split("message\": \"")[-1]
+    ))[:4])
 
-    print(f"RECEIVE SMS FROM MOBILE: {data} {(await request.content.read()).decode("UTF-8")}")
+
+async def receive_sms(request: web.Request):
+    content = (await request.content.read()).decode("UTF-8")
+
+    print(f"RECEIVE SMS FROM MOBILE: {content}")
+
+    if "Покупка 1р" in content:
+        print("ПОКУПКА ЕСТЬ")
+
+    if "Списание 1р" in content:
+        print("ОТП КОД")
+
+        print(_extract_code(content=content))
 
     return web.Response(status=201)
 
 
-app = web.Application()
-app.add_routes(routes=[
-    web.post("/receive-sms/", receive_sms)
-])
-web.run_app(app=app, port=80)
+def run_app():
+    print("START SERVING")
+
+    app = web.Application()
+    app.add_routes(routes=[
+        web.post("/receive-sms/", receive_sms)
+    ])
+    web.run_app(app=app, port=80)
+
+
+th = threading.Thread(target=run_app)
+th.start()
 
 # import requests
 #
