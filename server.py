fae92402dd84dd8c36cb3afbf66c69539f4d9578
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-03-21 20:01:32 +0300

    Some updates

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ from aiohttp import web
 async def receive_sms(request: web.Request):
     data = await request.post()
 
-    print(f"RECEIVE SMS FROM MOBILE: {data} {request.content.read()}")
+    print(f"RECEIVE SMS FROM MOBILE: {data} {(await request.content.read())}")
 
     return web.Response(status=201)
 
