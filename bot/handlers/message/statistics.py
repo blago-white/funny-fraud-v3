7b51f3b95defb67d0165c4dbc620d9104e38db14
Bogdan Loginov <bogdanloginov31@gmail.com>
2025-04-04 15:52:38 +0300

    Some updates

--- bot/handlers/message/statistics.py
+++ bot/handlers/message/statistics.py
@@ -25,10 +25,10 @@ async def show_statistics(
         chat_id=message.chat.id,
         text=STATISTICS_MESSAGE.format(
             total=total,
-            links=("\n".join([
+            links="\n".join([
                  f"{link} | <b>+{today_data[link]}</b>"
                  for link in today_data
-            ]) + "\n") if today_data else "",
+            ]) if today_data else "",
             balance_delta=sms_balance_delta,
             avg_price=abs(sms_balance_delta / total) if total and sms_balance_delta else 0
         )
