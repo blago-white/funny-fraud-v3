6178739ab59852f51be2c4be3a330a1ebbcb54d5
Bogdan Loginov <bogdanloginov31@gmail.com>
2026-01-23 15:00:56 +0300

    Updates

--- parser/utils/sms/herosms.py
+++ parser/utils/sms/herosms.py
@@ -22,7 +22,7 @@ class HeroSMSCodesService(BaseSmsService):
         result = requests.get(
             url="https://hero-sms.com/stubs/handler_api.php"
                 f"?api_key={self._apikey}&action=getNumber"
-                f"&service=atu&country={os.environ.get("SMS_COUNTRY")}&maxPrice=0.6"
+                f"&service=atu&country={os.environ.get("SMS_COUNTRY")}&maxPrice=0.5"
         ).text
 
         print("HERO SMS", result)
